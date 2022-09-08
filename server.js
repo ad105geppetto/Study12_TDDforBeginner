715d4d9800213a7edf730ee4b0b9efbb2f59e9c4
ad105geppetto <ad105geppetto@gmail.com>
2022-09-08 22:52:34 +0900

    feat: 라우터 추가

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const port = 5000;
 
 const app = express();
 
+const productRoutes = require("./routes");
+
 app.use(express.json());
+app.use("/api/products", productRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
