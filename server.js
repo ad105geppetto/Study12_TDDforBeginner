d467e43aa03d59e99af5cd0747291c65aa3a9ea3
ad105geppetto <ad105geppetto@gmail.com>
2022-09-10 14:16:37 +0900

    feat: MongoDB 연결

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const port = 5000;
 const app = express();
 
 const productRoutes = require("./routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@test.81mvvlc.mongodb.net/${process.env.MONGO_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
