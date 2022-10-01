41f7a444c393c805cefea515801e33c0134d5c23
ad105geppetto <ad105geppetto@gmail.com>
2022-10-02 00:33:52 +0900

    fix: getProducts 통합 테스트 수정
    # 내용
    - 테스트할때 데이터가 컨트롤러로 넘어오지 않았습니다.
    
    # 해결
    - 라우터를 작성했습니다.

--- routes.js
+++ routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require("./controllers/products");
 
 router.post("/", productController.createProduct);
+router.get("/", productController.getProduct);
 
 module.exports = router;
